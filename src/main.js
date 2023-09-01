a42623c4cf3feb7d451dbf79a4dc4a0168fcf4c0
Agustin Repetto <repettoagustin30@gmail.com>
2023-09-01 11:36:00 -0300

    Stefa

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ function hello() {
 function bye() {
   function hola(){
     function main(){
-      var pepe = console.log("");
+      var pepe = console.log("HOLAAAAAAAA");
+      return pepe;
+    }
+    function hola2(pepe){
+      console.log(pepe);
     }
   }
 
